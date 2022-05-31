794457ae6039aa3a49ee47c92232d25949fc2b63
Shiyun BATLLE <shiyun.rabbit@gmail.com>
2022-05-31 14:03:49 +0200

    pull seeds

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -1,2 +1,9 @@
 class SpotsController < ApplicationController
+  def index
+    if params[:query].present?
+      @spots = Spot.global_search(params[:query])
+    else
+      @spots = Spot.all
+    end
+  end
 end
--- app/models/spot.rb
+++ app/models/spot.rb
@@ -13,4 +13,14 @@ class Spot < ApplicationRecord
   validates :description, length: {minimum: 100}
   validates :type, inclusion: {in: TYPES}
   validates :category, inclusion: {in: CATEGORIES}, allow_blank: true
+
+  include PgSearch::Model
+  pg_search_scope :global_search,
+    against: [ :full_name, :address, :description, :type, :category ],
+    associated_against: {
+      activities: [ :name, :description ]
+    },
+    using: {
+      tsearch: { prefix: true }
+  }
 end
