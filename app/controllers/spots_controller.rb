a9a2d06561d5126b306d2c6e5ec334384184b766
Aurore Rigault <rigault.aurore@gmail.com>
2022-05-31 14:54:02 +0200

    show & commentaire d'un spot

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -3,4 +3,10 @@ class SpotsController < ApplicationController
   def show
     @spot = Spot.find(params[:id])
   end
+
+  def comments
+    @spot = Spot.find(params[:id])
+    @comments = @spot.comments
+    @comment = Comment.new
+  end
 end
