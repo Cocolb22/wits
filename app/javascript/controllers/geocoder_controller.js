ba12caf354533b0555bfcac54a33a65f766ab116
Le Bris Corentin <corentin.lebris@hotmail.fr>
2022-06-01 17:40:45 +0200

    delete a useless phrase

--- app/javascript/controllers/geocoder_controller.js
+++ app/javascript/controllers/geocoder_controller.js
@@ -6,7 +6,6 @@ export default class extends Controller {
   static targets = ["address"]
 
   connect() {
-    console.log("Hello from geocoder")
     this.geocoder = new MapboxGeocoder({
       accessToken: this.apiKeyValue,
       types: "country,region,place,postcode,locality,neighborhood,address"
