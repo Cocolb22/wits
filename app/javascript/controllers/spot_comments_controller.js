cf0073d79eb8d3d417a28a30072ff7357e994324
Aurore Rigault <rigault.aurore@gmail.com>
2022-06-01 17:23:59 +0200

    uppy up

--- app/javascript/controllers/spot_comments_controller.js
+++ app/javascript/controllers/spot_comments_controller.js
@@ -1,33 +1,47 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = ["form", "list", "btn", "rating"]
+  static targets = ["form", "list", "btn", "rating", "input"]
 
   connect() {
     console.log(this.formTarget)
     console.log(this.contentInputTarget)
-    console.log(this.ratingInputTarget)
+    console.log(this.inputTargets)
   }
 
   post(event) {
     event.preventDefault()
-    const url = this.formTarget.action
-    this.btnTarget.disabled = true
-    this.btnTarget.innerHTML = '<div class="loader"></div>'
-    fetch(url, {
-      method: "POST",
-      headers: { "Accept": "application/json" },
-      body: new FormData(this.formTarget)
+    let isValid = true
+
+    this.inputTargets.forEach((input) => {
+      if (!input.value.trim() || input.value.trim() === "") {
+        input.classList.add("invalid")
+        input.insertAdjacentHTML('afterend', '<p class="invalid-input">Ce champ doit être rempli.</p>')
+        input.focus()
+
+        isValid = false
+      }
     })
-      .then(response => response.json())
-      .then((data) => {
-        if (data.success) {
-          this.ratingTarget.outerHTML = data.rating
-          this.listTarget.insertAdjacentHTML('afterbegin', data.partial)
-          this.formTarget.outerHTML = data.form
-        } else {
-          this.formTarget.outerHTML = data.form
-        }
+
+    if (isValid) {
+      const url = this.formTarget.action
+      this.btnTarget.disabled = true
+      this.btnTarget.innerHTML = '<div class="loader"></div>'
+      fetch(url, {
+        method: "POST",
+        headers: { "Accept": "application/json" },
+        body: new FormData(this.formTarget)
       })
+        .then(response => response.json())
+        .then((data) => {
+          if (data.success) {
+            this.ratingTarget.outerHTML = data.rating
+            this.listTarget.insertAdjacentHTML('afterbegin', data.partial)
+            this.formTarget.outerHTML = data.form
+          } else {
+            this.formTarget.outerHTML = data.form
+          }
+        })
+    }
   }
 }
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,3 +14,81 @@ ActiveStorage.start()
 
 import "controllers"
 import "bootstrap"
+
+const Uppy = require('@uppy/core')
+const Dashboard = require('@uppy/dashboard')
+const ActiveStorageUpload = require('@excid3/uppy-activestorage-upload')
+const Webcam = require('@uppy/webcam')
+
+require('@uppy/core/dist/style.css')
+require('@uppy/dashboard/dist/style.css')
+
+document.addEventListener('turbolinks:load', () => {
+  document.querySelectorAll('[data-uppy]').forEach(element => setupUppy(element))
+})
+
+function setupUppy(element) {
+  let trigger = element.querySelector('[data-behavior="uppy-trigger"]')
+  let form = element.closest('form')
+  let direct_upload_url = document.querySelector('meta[name="direct-upload-url"]').getAttribute('content')
+  let field_name = element.dataset.uppy
+
+  trigger.addEventListener("click",(event) => event.preventDefault())
+
+  let uppy = new Uppy({
+    autoProceed: false,
+    allowMultipleUploadBatches: true,
+    logger: Uppy.debugLogger
+  })
+  uppy.use(ActiveStorageUpload, {
+    directUploadUrl: direct_upload_url
+  })
+
+  uppy.use(Dashboard, {
+    trigger: trigger,
+    closeAfterFinish: true,
+  })
+
+  uppy.use(Webcam, {
+    target: Dashboard,
+    onBeforeSnapshot: () => Promise.resolve(),
+    countdown: false,
+    modes: [
+      'picture',
+    ],
+    mirror: true,
+    preferredImageMimeType: null,
+    locale: {},
+  })
+
+  uppy.on('complete', (result) => {
+    element.querySelectorAll('[data-pending-upload]').forEach(element => element.parentNode.removeChild(element))
+
+    result.successful.forEach(file => {
+      appendUploadedFile(element, file, field_name)
+      setPreview(element, file)
+    })
+
+    uppy.reset()
+  })
+}
+
+function appendUploadedFile(element, file, field_name) {
+  const hiddenField = document.createElement('input')
+
+  hiddenField.setAttribute('type', 'hidden')
+  hiddenField.setAttribute('multiple', 'multiple')
+  hiddenField.setAttribute('name', field_name)
+  hiddenField.setAttribute('data-pending-upload', true)
+  hiddenField.setAttribute('value', file.response.signed_id)
+
+  element.appendChild(hiddenField)
+}
+
+function setPreview(element, file) {
+  const imgPreview = document.getElementById("imgPreview")
+  const imgPrev = document.createElement('img')
+
+  imgPrev.setAttribute('src', file.preview)
+  imgPreview.appendChild(imgPrev)
+}
