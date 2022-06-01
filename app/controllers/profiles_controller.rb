d73e7333fe1eb6386b84e5544d4ef6214094969c
Shiyun BATLLE <shiyun.rabbit@gmail.com>
2022-06-01 18:02:08 +0200

    profile_show + edit

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,25 @@
 class ProfilesController < ApplicationController
+  def show
+    @user = current_user
+  end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    if @user.update(profile_params)
+      redirect_to profile_path
+      flash[:notice] = "Profil modifié !"
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def profile_params
+    params.require(:user).permit(:last_name, :first_name, :nick_name, :address, :photo, :birthday, :gender, :favorite_area)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :favorites
   has_many :favorite_spots, through: :favorites, source: :spot
-  has_one_attached :status
+
   has_one_attached :photo
 
   validates :nick_name, presence: true, uniqueness: true
