1179f65babc0c6c42b0968a0048f8a1336fa7390
Le Bris Corentin <corentin.lebris@hotmail.fr>
2022-06-02 14:23:34 +0200

    add the user exp for the comments and photos

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,6 +6,16 @@ class CommentsController < ApplicationController
     @comment.spot = @spot
     @comment.user = current_user
 
+    unless current_user.comments.where(created_at: (Date.today.midnight..Date.tomorrow.midnight)).count >= 2
+      @comment.user.add_points_and_update_status(2)
+    end
+
+    unless current_user.comments.images.where(created_at: (Date.today.midnight..Date.tomorrow.midnight)).count >= 2
+      @comment.user.add_points_and_update_status(5)
+    end
+
+
+
     if @comment.save
       respond_to do |format|
         format.html { redirect_to comments_spot_path(@spot) }
@@ -28,6 +38,8 @@ class CommentsController < ApplicationController
       end
     end
   end
+
+
 
   private
 
