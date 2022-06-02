87e1da9c0a82470e92032afddf123b2d6ac8aac6
Shiyun BATLLE <shiyun.rabbit@gmail.com>
2022-06-02 17:24:27 +0200

    like et unlike comments

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,5 +1,4 @@
 class FavoritesController < ApplicationController
-
   def create
     @favorite = Favorite.new
 
--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   end
 
   resources :favorites, only: [:index, :destroy ]
+  resources :likes, only: [:destroy]
 
   resources :comments, only: [] do
     member do
