5d6ed2677087629368996bee02ecd3febda83db6
Aurore Rigault <rigault.aurore@gmail.com>
2022-06-03 13:13:12 +0200

    beginning weathear page

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -33,4 +33,9 @@ class SpotsController < ApplicationController
     @comments = @spot.comments.order('id DESC')
     @comment = Comment.new
   end
+
+  def forecast
+    @spot = Spot.find(params[:id])
+    @weather = @spot.weathers
+  end
 end
