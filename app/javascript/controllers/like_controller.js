1c6ce900c004ffbed90261e13454aa2da0cac910
Shiyun BATLLE <shiyun.rabbit@gmail.com>
2022-06-07 12:38:03 +0200

    like-animations comments

--- app/javascript/controllers/like_controller.js
+++ app/javascript/controllers/like_controller.js
@@ -5,9 +5,30 @@ export default class extends Controller {
   static targets = ["button"]
   connect() {
   }
+
   like(event) {
     event.preventDefault()
     event.stopPropagation()
+    if (event.currentTarget.querySelector('.far')) {
+      const burst = new mojs.Burst({
+        left: -10, top: 0,
+        radius: { 4: 40},
+        count: 14,
+        timeline: {delay: 100},
+        children: {
+          radius: 3,
+          fill: '#5FC1FA',
+          scale: {1 : 0, easing: 'quad.in'},
+          pathScale: [.8, null],
+          degreeShift: [300, null],
+          duration: [500, 700],
+          easing: 'quint.out'
+        }
+      });
+      burst
+      .tune({ x: event.pageX, y: event.pageY })
+      .replay();
+    }
 
     const url = event.currentTarget.attributes['href'].value
     const method = event.currentTarget.dataset.method
