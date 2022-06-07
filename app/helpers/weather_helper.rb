a61056b92a6ddda5fe444cc8ecce8e14e9448e4c
Aurore Rigault <rigault.aurore@gmail.com>
2022-06-07 12:21:40 +0200

    meteooooo

---
+++ app/helpers/weather_helper.rb
@@ -0,0 +1,18 @@
+module WeatherHelper
+
+  def get_picto_for(weather)
+    if weather.match?(/(nuageux|couvert|voilé)/i)
+      return image_tag 'icons/clouds.png'
+    elsif weather.match?(/(pluie|bruine|averse)/i)
+      return image_tag 'icons/rain.png'
+    elsif weather.match?(/neige/i)
+      return image_tag 'icons/neige.png'
+    elsif weather.match?(/orage/i)
+      return image_tag 'icons/thunder.png'
+    elsif weather.match?(/brouillard/i)
+      return image_tag 'icons/brouillard.png'
+    else
+      return image_tag 'icons/sun.png'
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -507,18 +507,18 @@ plage_toul_gwenn.save!
 
 TOKEN = "d94e51c82dfac84ee6e9e938fd9b66dd18dcddeab62d0eda61cd3ca9e99842a0"
 
-CODES_TEMPS = ["Soleil", "Peu nuageux", "Ciel voilé", "Nuageux", "Très nuageux", "Couvert", "Brouillard", "Brouillard givrant", "Pluie faible", "Pluie modérée", "Pluie forte", "Pluie faible verglaçante", "Pluie modérée verglaçante",
-                  "Pluie forte verglaçante", "Bruine", "Neige faible", "Neige modérée", "Neige forte", "Pluie et neige mêlées faibles", "Pluie et neige mêlées modérées", "Pluie et neige mêlées fortes", "Averses de pluie locales et faibles",
-                  "Averses de pluie locales", "Averses locales et fortes", "Averses de pluie faibles", "Averses de pluie", "Averses de pluie fortes", "Averses de pluie faibles et fréquentes", "Averses de pluie fréquentes",
-                  "Averses de pluie fortes et fréquentes", "Averses de neige localisées et faibles", "Averses de neige localisées", "Averses de neige localisées et fortes", "Averses de neige faibles", "Averses de neige", "Averses de neige fortes",
-                  "Averses de neige faibles et fréquentes", "Averses de neige fréquentes", "Averses de neige fortes et fréquentes", "Averses de pluie et neige mêlées localisées et faibles", "Averses de pluie et neige mêlées localisées",
-                  "Averses de pluie et neige mêlées localisées et fortes", "Averses de pluie et neige mêlées faibles", "Averses de pluie et neige mêlées", "Averses de pluie et neige mêlées fortes", "Averses de pluie et neige mêlées faibles et nombreuses",
-                  "Averses de pluie et neige mêlées fréquentes", "Averses de pluie et neige mêlées fortes et fréquentes", "Orages faibles et locaux", "Orages locaux", "Orages fort et locaux", "Orages faibles", "Orages", "Orages forts", "Orages faibles et fréquents",
-                  "Orages fréquents", "Orages forts et fréquents", "Orages faibles et locaux de neige ou grésil", "Orages locaux de neige ou grésil", "Orages locaux de neige ou grésil", "Orages faibles de neige ou grésil", "Orages de neige ou grésil", "Orages de neige ou grésil",
-                  "Orages faibles et fréquents de neige ou grésil", "Orages fréquents de neige ou grésil", "Orages fréquents de neige ou grésil", "Orages faibles et locaux de pluie et neige mêlées ou grésil", "Orages locaux de pluie et neige mêlées ou grésil",
-                  "Orages fort et locaux de pluie et neige mêlées ou grésil", "Orages faibles de pluie et neige mêlées ou grésil", "Orages de pluie et neige mêlées ou grésil", "Orages forts de pluie et neige mêlées ou grésil", "Orages faibles et fréquents de pluie et neige mêlées ou grésil",
-                  "Orages fréquents de pluie et neige mêlées ou grésil", "Orages forts et fréquents de pluie et neige mêlées ou grésil", "Pluies orageuses", "Pluie et neige mêlées à caractère orageux", "Neige à caractère orageux", "Pluie faible intermittente", "Pluie modérée intermittente",
-                  "Pluie forte intermittente", "Neige faible intermittente", "Neige modérée intermittente", "Neige forte intermittente", "Pluie et neige mêlées", "Pluie et neige mêlées", "Pluie et neige mêlées", "Averses de grêle"]
+CODES_TEMPS = ["Soleil", "Peu nuageux", "Ciel voilé", "Nuageux", "Très nuageux", "Couvert", "Brouillard", "Brouillard", "Pluie faible", "Pluie modérée", "Pluie forte", "Pluie faible", "Pluie modérée",
+                  "Pluie forte", "Bruine", "Neige faible", "Neige modérée", "Neige forte", "Pluie", "Pluie", "Pluie", "Averses",
+                  "Averses", "Averses", "Averses", "Averses", "Averses", "Averses", "Averses",
+                  "Averses", "Averses", "Averses", "Averses", "Averses", "Averses", "Averses",
+                  "Averses", "Averses", "Averses", "Averses", "Averses",
+                  "Averses", "Averses", "Averses", "Averses", "Averses",
+                  "Averses", "Averses", "Orages", "Orages", "Orages fort", "Orages", "Orages", "Orages", "Orages",
+                  "Orages", "Orages", "Orages", "Orages", "Orages", "Orages", "Orages", "Orages",
+                  "Orages", "Orages", "Orages", "Orages", "Orages",
+                  "Orages", "Orages", "Orages", "Orages", "Orages",
+                  "Orages", "Orages", "Pluies orageuses", "Pluie orageuses", "Neige orageuse", "Pluie faible", "Pluie modérée",
+                  "Pluie forte", "Neige faible", "Neige modérée", "Neige forte", "Pluie", "Pluie", "Pluie", "Averses"]
 
 
 Spot.all.each do |spot|
@@ -528,7 +528,6 @@ Spot.all.each do |spot|
     city, forecasts = JSON.parse(stream.read).values_at('city','forecast')
 
     forecasts.each do |forecast|
-
       Weather.create!(
         spot: spot,
         day: forecast["day"],
